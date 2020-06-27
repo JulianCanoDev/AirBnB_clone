9317672f9d1cd66f010bc7bfdf32ebdd2b0cf983
julian <juliancano.dev@gmail.com>
2020-06-27 17:39:27 -0500

    permissions

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+import uuid
+from datetime import datetime
+import models
+
+class BaseModel():
+    """ base class model for AirBnB objects
+    Attributes:
+        id: str, unique UUID of instance
+        created_at: datetime, time of instance creation
+        updated_at: datetime, instance update time
+    """
+    def __init__(self, *args, **kwargs):
+        """ initializes class with id and created at time"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = \
+                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = value
+        else:
+            models.storage.new(self)
+
+    def __str__(self):
+        """Updates instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Save a state"""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """To directory"""
+        json_dict = {}
+        for key, value in self.__dict__.items():
+            json_dict[key] = value
+        json_dict['__class__'] = self.__class__.__name__
+        json_dict['created_at'] = self.created_at.isoformat()
+        json_dict['updated_at'] = self.updated_at.isoformat()
+        return json_dict
